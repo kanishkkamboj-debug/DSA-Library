43417db48dca345bc3cd68a0285e362c84d90861
Kanishk Kamboj <24bcs11487@cuchd.in>
2026-07-16 15:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (35.09%) - LeetSync

--- LeetCode/58-length-of-last-word/length-of-last-word.cpp
+++ LeetCode/58-length-of-last-word/length-of-last-word.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int count = 0;
         int i = s.size() - 1;
         while (i >= 0 && s[i] == ' ') {
             i--;
         }
+        int count = 0;
         while (i >= 0 && s[i] != ' ') {
             count++;
             i--;
