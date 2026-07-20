fdbc671c7b74ebc013900e8df1269ab27dba3c1b
Kanishk Kamboj <24bcs11487@cuchd.in>
2026-07-20 18:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (19.91%) - LeetSync

--- LeetCode/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ LeetCode/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    int maxDepth(TreeNode* root) { return ans(root); }
-    int ans(TreeNode* root) {
+    int maxDepth(TreeNode* root) {
         if (root == nullptr) {
             return 0;
         }
-        return 1 + max(ans(root->left), ans(root->right));
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
