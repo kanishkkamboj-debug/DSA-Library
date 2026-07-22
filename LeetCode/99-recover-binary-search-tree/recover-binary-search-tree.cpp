286b1af293b9ea023365398fd9ee548673df5fb0
Kanishk Kamboj <24bcs11487@cuchd.in>
2026-07-22 17:25:57 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (79.61%) - LeetSync

--- LeetCode/99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ LeetCode/99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    vector<TreeNode*> nums;
+    TreeNode* prev = nullptr;
+    TreeNode* first = nullptr;
+    TreeNode* second = nullptr;
+
     void arr(TreeNode* root) {
         if (root == nullptr) {
             return;
         }
         arr(root->left);
-        nums.push_back(root);
+        if (prev != nullptr && root->val < prev->val) {
+            if (first == nullptr) {
+                first = prev;
+            }
+            second = root;
+        }
+        prev = root;
         arr(root->right);
     }
     void recoverTree(TreeNode* root) {
         arr(root);
-        int galat = 0;
-        TreeNode* prev = nullptr;
-        TreeNode* galat1f = nullptr;
-        TreeNode* galat1s = nullptr;
-        TreeNode* galat2f = nullptr;
-        TreeNode* galat2s = nullptr;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i]->val > nums[i + 1]->val) {
-                if (galat == 0) {
-                    galat1f = nums[i];
-                    galat1s = nums[i + 1];
-                    galat++;
-                } else {
-                    galat2f = nums[i];
-                    galat2s = nums[i + 1];
-                    galat++;
-                }
-            }
-        }
-        if (galat == 1) {
-            swap(galat1f->val, galat1s->val);
-        } else if (galat == 2) {
-            swap(galat1f->val, galat2s->val);
-        }
+        int temp = first->val;
+        first->val = second->val;
+        second->val = temp;
     }
 };
