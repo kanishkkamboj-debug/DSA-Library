7fb701c8c0c1bc116e0d7de526fd45d1069a5e5e
Kanishk Kamboj <24bcs11487@cuchd.in>
2026-07-30 23:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (95.83%) - LeetSync

--- LeetCode/3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ LeetCode/3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> freq(26);
-        for (auto val : word) {
-            freq[val - 'a']++;
-        }
-        sort(freq.rbegin(), freq.rend());
         int count = 0;
-        for (int i = 0; i < word.size(); i++) {
-            count += freq[i] * (1 + (i / 8));
-        }
+        for (int i = 0; i < word.size(); i++) count += (1 + (i / 8));
         return count;
     }
 };
