f70f274e84f656d191d41b11b573e9f5ee509abb
Kanishk Kamboj <24bcs11487@cuchd.in>
2026-08-14 21:28:49 +0530

    Time: 67 ms (42.15%) | Memory: 149.2 MB (83.34%) - LeetSync

--- LeetCode/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ LeetCode/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
-        int maxi = 0;
+    int maxSubarrayLength(vector<int>& nums, int k) {  
+        //this code doesn't have that -a problem because it is int not string
         unordered_map<int, int> count;
+        int maxi = 0;
         int left = 0;
         for (int right = 0; right < nums.size(); right++) {
-            count[nums[right] - 'a']++;
-            while (count[nums[right] - 'a'] > k) {
-                count[nums[left] - 'a']--;
+            count[nums[right] ]++;
+            while (count[nums[right]] > k) {
+                count[nums[left] ]--;
                 left++;
             }
             maxi = max(maxi, right - left + 1);
