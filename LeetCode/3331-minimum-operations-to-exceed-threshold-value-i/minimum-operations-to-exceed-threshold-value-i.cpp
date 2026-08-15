13d5bfbc503c35cf98988787ea2ee87970a73625
Kanishk Kamboj <24bcs11487@cuchd.in>
2026-08-15 23:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (80.66%) - LeetSync

---
+++ LeetCode/3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int count = 0;
+        for (auto val : nums) {
+            if (val < k)
+                count++;
+        }
+        return count;
+    }
+};
